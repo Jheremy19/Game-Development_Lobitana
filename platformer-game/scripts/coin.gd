c29225c27a76a3013d3334970ef221530c16081a
Jheremy19 <158053851+Jheremy19@users.noreply.github.com>
2026-03-06 21:46:40 +0800

    Add files via upload

--- platformer-game/scripts/coin.gd
+++ platformer-game/scripts/coin.gd
@@ -1,6 +1,12 @@
 extends Area2D
 
+@onready var game_manager: Node = %"Game Manager"
+@onready var coin_sfx: AudioStreamPlayer2D = $"Coin SFX"
 
-func _on_body_entered(body: Node2D) -> void:
-	print("+1 coin!")
+func _on_body_entered(_body: Node2D) -> void:
+	game_manager.add_point()
+	coin_sfx.play()
+	hide()
+	set_deferred("monitoring", false)
+	await coin_sfx.finished
 	queue_free()
